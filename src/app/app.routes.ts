e9fff253540dc9344318c72d413c4c091bc675e3
William Zamora <williamzv@gmail.com>
2018-09-03 17:34:21 -0600

    correccion para Produccción

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,8 @@ const APP_ROUTES: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'heroe/:id', component: HeroeComponent },
   { path: 'buscar/:key', component: BuscadorComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
