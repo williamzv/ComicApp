bf23ab7febfee9279df82413fe44de9425de97cc
William Zamora <williamzv@gmail.com>
2018-09-03 18:00:33 -0600

    para Produccion version final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,4 +15,4 @@ const APP_ROUTES: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' }
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
